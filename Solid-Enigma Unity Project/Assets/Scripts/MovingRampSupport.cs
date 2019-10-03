883919d133162c4c284911191ff2191424fe482e
Fobbles <55261916+Fobbles@users.noreply.github.com>
2019-10-03 18:12:49 -0500

    created a moning ramp support that moves with player input

---
+++ Solid-Enigma Unity Project/Assets/Scripts/MovingRampSupport.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRampSupport : MonoBehaviour
+{
+    private float moveSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the player presses up arrow, 
+        // we move the sqare up.
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, moveSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -moveSpeed, 0);
+        }
+        
+    }
+}
