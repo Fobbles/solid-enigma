1909e9413ac491f66fdca720dedaa31d42def15d
Fobbles <55261916+Fobbles@users.noreply.github.com>
2019-11-07 17:56:07 -0600

    Added The "You Win," goal sound effects, and background music.

--- Solid-Enigma Unity Project/Assets/Scripts/Goal.cs
+++ Solid-Enigma Unity Project/Assets/Scripts/Goal.cs
@@ -6,11 +6,23 @@ using UnityEngine;
 /// </summary>
 public class Goal : MonoBehaviour
 {
+    public GameObject winText;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Touched Trigger.");
         if (collision.tag == "Player")
         {
+            winText.SetActive(true);
+            // cue sound effect
+            audioSource.Play();
             Debug.Log("The Player Has Entered The Goal!");
         }
     }
