244af614eca1007176d0a74f4fcc3f25b0d8c3ae
Fobbles <55261916+Fobbles@users.noreply.github.com>
2019-11-14 17:09:11 -0600

    Added Obastacle Function

---
+++ Solid-Enigma Unity Project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public GameObject gamOverText;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched Trigger.");
+        if (collision.tag == "Player")
+        {
+            gamOverText.SetActive(true);
+            // cue sound effect
+            audioSource.Play();
+            Debug.Log("The Player Has Touched The Obstical!");
+        }
+    }
+}
